27968ae430d2a151655aa1d76f94c193e76cd379
Gurasuraisu <dakirb@proton.me>
2025-03-27 21:04:32 +0000

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -1,11 +1,13 @@
 const LANG_EN = {
     GREETING: "Gurasuraisu loaded successfully",
+    LICENCE: "Gurasuraisu is made by kirbIndustries, and is licensed under the GNU General Public License, Version 2.0 (GPL-2.0)",
     WELCOME_MESSAGE: "Hello, welcome to Gurasuraisu!",
     // Add other translations...
 };
 
 const LANG_JP = {
     GREETING: "ぐらするらいす読み込み成功",
+    LICENCE: "ぐらするらいすはkirbIndustriesによって作られ、GNU General Public License, Version 2.0 (GPL-2.0)の下でライセンスされています。",
     WELCOME_MESSAGE: "こんにちは、ぐらすりすへようこそ！",
     // Add other translations...
 };
