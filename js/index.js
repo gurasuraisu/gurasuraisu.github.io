495b59ca88f2db4553c35a6f7234b938c1a30f95
Gurasuraisu <dakirb@proton.me>
2025-03-29 12:25:08 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ function applyLanguage(language) {
     document.querySelector('.font-selection .cust-label').childNodes[2].textContent = language.STYLE;
     document.getElementById('language-label').textContent = language.LANGPICK;
     document.querySelector('.version-info button#versionButton').textContent = language.DETAIL_VERS;
-    document.getElementById('reset-label').textContent = language.RESET;
+    document.getElementById('language-label').textContent = language.RESET;
     document.querySelector('.reset-settings button#resetButton').textContent = language.RESET_BTN;
 	
     // Updating font selection options
